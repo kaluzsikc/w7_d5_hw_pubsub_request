4ff290edce795d6c802aad9a35dbd1ae27bdbd1e
kaluzsikc <kaluzsikc@gmail.com>
2018-09-16 11:22:30 +0100

    Beers model created

---
+++ src/models/beers.js
@@ -0,0 +1,20 @@
+const PubSub = require('../helpers/pub_sub.js');
+const RequestHelper = require('../helpers/request_helpers.js');
+
+const Beers = function(){
+
+  this.beers = [];
+
+};
+
+
+Beers.prototype.getData = function () {
+  const requestHelper = new RequestHelper('https://api.punkapi.com/v2/beers')
+  requestHelper.get((data) => {
+    PubSub.publish('Beers:beers-ready', data);
+    console.log('Data to be published:', data);
+  });
+}
+
+
+module.exports = Beers;
